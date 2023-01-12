c5db6c6006ad3709c6f4251816d9a26034c68893
Nilesh Patwal <nileshpatwal786@gmail.com>
2023-01-12 23:58:27 +0530

    updated

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const https = require("https");
 const bodParser = require("body-parser");
 const { userInfo } = require("os");
 const app = express();
+app.use(express.static("public"));
 app.use(bodParser.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -11,14 +12,14 @@ app.get("/", function (req, res) {
 app.post("/", function (req, res) {
   const city = req.body.cityName;
   const apikey = "63f42f7eda908ad6c0fa9697a8c8a985";
-  const unit = "imperial";
+  const units = "metric";
   const url =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     city +
     "&appid=" +
     apikey +
     "&units=" +
-    unit;
+    units;
   https.get(url, function (response) {
     response.on("data", function (data) {
       const weatherData = JSON.parse(data);
@@ -26,8 +27,17 @@ app.post("/", function (req, res) {
       const weatherdiscription = weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
       const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-      res.write("<b>The Weather is currently " + weatherdiscription + "</b>");
-      res.write("<h1>The Temperature in " + city + " is "+temp + " Degrees Fahrenheit</h1>"
+      res.write(
+        "<font size=20px><b>The Weather is currently " +
+          weatherdiscription +
+          "</b></font>"
+      );
+      res.write(
+        "<font size=100px><h1>The Temperature in " +
+          city +
+          " is " +
+          temp +
+          " &deg;C</h1></font>"
       );
       res.write("<img src=" + imageURL + ">");
       res.send();
